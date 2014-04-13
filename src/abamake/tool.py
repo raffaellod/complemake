7c424bf69bc059eb27b1a50eac6f8a251c2f6f7b
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2014-04-13 13:16:06 -0400

    Suppress “Creating library” message printed by link.exe

--- src/abamake/tool.py
+++ src/abamake/tool.py
@@ -865,6 +865,13 @@ class MsLinker(Linker):
       # link.exe logs to stdout instead of stderr.
       dictPopenArgs['stderr'] = subprocess.STDOUT
 
+      # link.exe has the annoying habit of printing the file name we asked it to compile; create a
+      # filtered logger to hide it.
+      log = make.logging.FilteredLogger(log)
+      log.add_exclusion('   Creating library {0}.lib and object {0}.exp'.format(
+         os.path.splitext(self._m_sOutputFilePath)[0]
+      ))
+
       return Linker._create_job_instance(self, iterQuietCmd, dictPopenArgs, log, sStdErrFilePath)
 
 
