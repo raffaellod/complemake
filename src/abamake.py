451837936f4e7b412fd22f439fa590b02ffcc96c
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2014-12-16 08:37:34 -0500

    Replace Abamake’s custom command-line parser with argparser

--- src/abamake.py
+++ src/abamake.py
@@ -39,37 +39,6 @@ import abamake
 ####################################################################################################
 # Globals
 
-def helptext(sInvalidArg):
-   """Displays a message with instructions on how to invoke Abamake. This function does not return.
-
-   str sInvalidArg
-      Invalid argument that prompted to show this message, or None if --help was specified on the
-      command line.
-   """
-
-   import textwrap
-   if sInvalidArg:
-      fnWrite = sys.stderr.write
-      fnWrite('error: unknown option argument: {}\n'.format(sInvalidArg))
-   else:
-      fnWrite = sys.stdout.write
-   fnWrite(textwrap.dedent("""\
-      Usage: abamake.py [options] [makefile] [targets...]
-      Options:
-      -f, --force-build   Unconditionally rebuild targets.
-      -j [N], --jobs[=N]  Build using N processes at at time; if N is omitted,
-                          build all independent targets at the same time. If not
-                          specified, the default is --jobs=<number of processors>.
-      -k, --keep-going    Continue building targets even if other independent
-                          targets fail.
-      -n, --dry-run       Don’t actually run any external commands. Useful to test
-                          if anything needs to be built.
-      -t, --force-test    Unconditionally run all test targets.
-      -v, --verbose       Increase verbosity level; can be specified multiple
-                          times.
-   """))
-   sys.exit(1 if sInvalidArg else 0)
-
 def main(iterArgs):
    """Implementation of __main__.
 
@@ -79,75 +48,64 @@ def main(iterArgs):
       Command return status.
    """
 
+   import argparse
+
+   argparser = argparse.ArgumentParser(add_help = False)
+   #   Usage: abamake.py [options] [makefile] [targets...]
+   argparser.add_argument(
+      '-n', '--dry-run', action = 'store_true', default = False,
+      help = 'Don’t actually run any external commands. Useful to test if anything needs to be ' +
+             'built.'
+   )
+   argparser.add_argument(
+      '-f', '--force-build', action = 'store_true', default = False,
+      help = 'Unconditionally rebuild all targets.'
+   )
+   argparser.add_argument(
+      '-t', '--force-test', action = 'store_true', default = False,
+      help = 'Unconditionally run all test targets.'
+   )
+   argparser.add_argument(
+      '--help', action = 'help',
+      help = 'Show this informative message and exit.'
+   )
+   argparser.add_argument(
+      '-j', '--jobs', default = 999999, metavar = 'N', type = int,
+      help = 'Build using N processes at at time; if N is omitted, build all independent targets ' +
+             'at the same time. If not specified, the default is --jobs <number of processors>.'
+   )
+   argparser.add_argument(
+      '-k', '--keep-going', action = 'store_true', default = False,
+      help = 'Continue building targets even if other independent targets fail.'
+   )
+   argparser.add_argument(
+      '-v', '--verbose', action = 'count', default = 0,
+      help = 'Increase verbosity level; can be specified multiple times.'
+   )
+   argparser.add_argument(
+      '-m', '--makefile', metavar = 'PROJECT.ABAMK', nargs = 1,
+      help = 'Abamakefile (.abamk) containing instructions on how to build targets. If omitted ' +
+             'and the current directory contains a single file matching *.abamk, that file will ' +
+             'be used as makefile.'
+   )
+   argparser.add_argument(
+      'target', nargs = '*',
+      help = 'List of target files to be conditionally built. If none are specified, all targets ' +
+             'declared in the Abamakefile (.abamk) will be conditionally built.'
+   )
+
+   args = argparser.parse_args()
+
    mk = abamake.Make()
-   iArg = 1
-   iArgEnd = len(iterArgs)
+   mk.job_controller.dry_run          = args.dry_run
+   mk.job_controller.force_build      = args.force_build
+   mk.job_controller.force_test       = args.force_test
+   mk.job_controller.running_jobs_max = args.jobs
+   mk.job_controller.keep_going       = args.keep_going
+   mk.log.verbosity                   += args.verbose
 
-   # Parse arguments, looking for option flags.
-   while iArg < iArgEnd:
-      sArg = iterArgs[iArg]
-      if sArg.startswith('--'):
-         if sArg == '--dry-run':
-            mk.job_controller.dry_run = True
-         elif sArg == '--force-build':
-            mk.job_controller.force_build = True
-         elif sArg == '--force-test':
-            mk.job_controller.force_test = True
-         elif sArg == '--help':
-            helptext(None)
-         elif sArg.startswith('--jobs'):
-            if sArg[len('--jobs')] == '=':
-               cJobs = int(sArg[len('--jobs') + 1:])
-            else:
-               cJobs = 999999
-            mk.job_controller.running_jobs_max = cJobs
-         elif sArg == '--keep-going':
-            mk.job_controller.keep_going = True
-         elif sArg == '--verbose':
-            mk.log.verbosity += 1
-         else:
-            helptext(sArg)
-      elif sArg.startswith('-'):
-         ich = 1
-         ichEnd = len(sArg)
-         while ich < ichEnd:
-            sArgChar = sArg[ich]
-            if sArgChar == 'f':
-               mk.job_controller.force_build = True
-            elif sArgChar == 'j':
-               # TODO: make this parsing more generic and more flexible.
-               ichNumberLast = ich + 1
-               while ichNumberLast < ichEnd and sArg[ichNumberLast] in '0123456789':
-                  ichNumberLast += 1
-               if ichNumberLast > ich + 1:
-                  cJobs = int(sArg[ich + 1:ichNumberLast])
-               else:
-                  cJobs = 999999
-               mk.job_controller.running_jobs_max = cJobs
-            elif sArgChar == 'k':
-               mk.job_controller.keep_going = True
-            elif sArgChar == 'n':
-               mk.job_controller.dry_run = True
-            elif sArgChar == 't':
-               mk.job_controller.force_test = True
-            elif sArgChar == 'v':
-               mk.log.verbosity += 1
-            else:
-               helptext(sArg)
-            ich += 1
-      else:
-         break
-      iArg += 1
-
-   # Check for a makefile name.
-   sMakefilePath = None
-   if iArg < iArgEnd:
-      sArg = iterArgs[iArg]
-      if sArg.endswith('.abamk'):
-         # Save the argument as the makefile path and consume it.
-         sMakefilePath = sArg
-         iArg += 1
-   # No makefile specified?
+   # Check for a makefile.
+   sMakefilePath = args.makefile
    if not sMakefilePath:
       # Check if the current directory contains a single Abamakefile.
       for sFilePath in os.listdir(os.getcwd()):
@@ -155,31 +113,31 @@ def main(iterArgs):
             if sMakefilePath:
                sys.stderr.write(
                   'error: multiple makefiles found in the current directory, please specify one ' +
-                     'explicitly\n'
+                  'explicitly with --makefile FILE.ABAMK\n'
                )
                return 1
             sMakefilePath = sFilePath
+      del sFilePath
       # Still no makefile?
       if not sMakefilePath:
-         sys.stderr.write('error: no makefile specified\n')
+         sys.stderr.write(
+            'error: no makefiles in current directory, please specify one explicitly with ' +
+            '--makefile FILE.ABAMK\n'
+         )
          return 1
-
    # Load the makefile.
    mk.parse(sMakefilePath)
-
 #   mk.print_target_graphs()
 
-   # If there are more argument, they will be treated as target named, indicating that only a subset
-   # of the targets should be built; otherwise all named targets will be built.
-   if iArg < iArgEnd:
+   # If any targets were specified, only a subset of the targets should be built; otherwise all
+   # named targets will be built.
+   if args.target:
       iterTargets = []
-      while iArg < iArgEnd:
-         sArg = iterArgs[iArg]
+      for sTarget in args.target:
          # mk.get_file_target() will raise an exception if no such file target is defined.
          iterTargets.append(
-            mk.get_named_target(sArg, None) or mk.get_file_target(os.path.normpath(sArg))
+            mk.get_named_target(sTarget, None) or mk.get_file_target(os.path.normpath(sTarget))
          )
-         iArg += 1
    else:
       iterTargets = mk.named_targets
 
